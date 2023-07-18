c573d2a0a6e0e5e21e84e5def3e80404b1799840
irenehl <26irenelopez@gmail.com>
2023-07-17 22:41:23 -0600

    feat: add user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,7 @@
 import { Prisma, PrismaClient } from '@prisma/client';
+import { UserDto } from './user.dto';
+import { HttpError } from '../common/http-error';
+import bcrypt from 'bcrypt';
 
 export class UserService {
     private readonly prisma: PrismaClient;
@@ -7,7 +10,29 @@ export class UserService {
         this.prisma = new PrismaClient();
     }
 
-    async register(data: Prisma.UserCreateInput) {
-        
+    async register(data: Prisma.UserCreateInput): Promise<UserDto> {
+        const alreadyExists = await this.prisma.user.findUnique({
+            where: { email: data.email },
+        });
+
+        if (alreadyExists) throw new HttpError(409, 'Account already exists');
+
+        const hashedPwd = await bcrypt.hash(
+            data.password,
+            Number(process.env.SALT ?? 10)
+        );
+
+        return await this.prisma.user.create({
+            data: {
+                ...data,
+                password: hashedPwd,
+            },
+        });
+    }
+
+    async findOne(where: Prisma.UserWhereUniqueInput): Promise<UserDto | null> {
+        return await this.prisma.user.findUniqueOrThrow({ where }).catch(() => {
+            throw new HttpError(404, 'User not found');
+        });
     }
 }
